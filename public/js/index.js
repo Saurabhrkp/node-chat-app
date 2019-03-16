b8c0622777fc14d8e13ffe5d387badd3a8875aaa
SAURABH PATEL <saurabhpatel230@gmail.com>
2019-03-16 14:47:40 +0530

    Added CSS

--- public/js/index.js
+++ public/js/index.js
@@ -28,16 +28,18 @@ socket.on('newLocationMessage', function (message) {
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
-})
+});
+
+var messageTextbox = jQuery('[name=message]');
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('')
   });
 });
 
@@ -47,13 +49,17 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by broswer.')
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location..');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     console.log(position)
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location');
   });
 });
